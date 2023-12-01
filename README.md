8ef95263a33c901600cc66909c0604f86394ffda
Mario Hamann <mariovhamann@gmail.com>
2023-12-01 20:24:42 +0100

    refactor fetch, middleware and preview route

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# The Storyblok Astro Ultimate Tutorial: Part 1
+# Storyblok x Astro Integration
+
+## The `preview` route
+
+1. In Storyblok setup `https://localhost:4321/storyblok-preview` as Preview URL and select it. From now on every requests should go through this wildcard route.
+2. The page doesn't contain any content, but instead fetches the requested page from Astro. In the fetch request, `https://localhost:4321/storyblok-preview/whatever...` is automatically altered to `https://localhost:4321/whatever...`.
--- src/middleware.js
+++ src/middleware.js
@@ -1,31 +1,41 @@
 export function onRequest({ locals, request, context, params }, next) {
-  // intercept response data from a request
-  // optionally, transform the response by modifying `locals`
+  // Helper to extract the body from the request in an usable form
+  function getRequestBody(req) {
+    const symbols = Object.getOwnPropertySymbols(req);
+    let body;
 
-  const symbols = Object.getOwnPropertySymbols(request);
-  let body;
-  for (const sym of symbols) {
-    if (request[sym].hasOwnProperty('body')) {
-      body = request[sym].body;
-      break;
+    // Find and extract the body from the request
+    for (const sym of symbols) {
+      if (req[sym].hasOwnProperty('body')) {
+        body = req[sym].body;
+        break;
+      }
+    }
+
+    // Safely retrieves a nested property from an object
+    function getNestedProperty(obj, ...props) {
+      return props.reduce((prev, prop) => (prev && prev[prop]) ? prev[prop] : null, obj);
+    }
+
+    // Retrieve and process the 'source' property
+    const source = getNestedProperty(body, 'source');
+    if (source) {
+      const decoder = new TextDecoder();
+      const decodedString = decoder.decode(source);
+      return JSON.parse(decodedString);
+    }
+
+    return null;
+  }
+
+  // Process the request and update locals if necessary
+  if (request['method'] === 'POST') {
+    const requestBody = getRequestBody(request);
+
+    if (requestBody && requestBody['is_storyblok_preview']) {
+      locals['_storybook_preview_data'] = requestBody;
     }
   }
 
-  function getNestedProperty(obj, ...props) {
-    return props.reduce((prev, prop) => (prev && prev[prop]) ? prev[prop] : null, obj);
-  }
-
-  const source = getNestedProperty(body, 'source');
-
-  if (source) {
-    // console.log(source);
-    const decoder = new TextDecoder();
-    const decodedString = decoder.decode(source);
-
-    locals['_storybook_preview'] = JSON.parse(decodedString);
-  } else {
-    // console.log('Source property does not exist');
-  }
-
   return next();
-};
+}
